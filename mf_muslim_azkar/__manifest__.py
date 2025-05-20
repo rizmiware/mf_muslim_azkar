120ffbcbfd261c4b6f93786fc15bad72ff49a3d7
mfhm95 <mohmd.foad95@gmail.com>
2025-05-21 00:18:54 +0300

    [IMP] banner for odoo apps

--- mf_muslim_azkar/__manifest__.py
+++ mf_muslim_azkar/__manifest__.py
@@ -24,12 +24,12 @@
 
     'author': "Mohamed Fouad",
     'website': "https://rizmiware.com",
-    'images': ['static/description/icon.png'],
+    'images': ['static/description/banner.png', 'static/description/icon.png'],
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Extra tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
