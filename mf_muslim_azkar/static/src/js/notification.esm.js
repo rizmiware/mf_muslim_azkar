4c7e15bac4e77fa354af264d35ecffdd62a81617
osamawaheed <ososwaheed45@gmail.com>
2025-06-16 18:38:11 +0300

    update_V_17

--- mf_muslim_azkar/static/src/js/notification.esm.js
+++ mf_muslim_azkar/static/src/js/notification.esm.js
@@ -1,9 +1,8 @@
-/** @odoo-module */
-import {Notification} from "@web/core/notifications/notification";
-import {patch} from "web.utils";
+/** @odoo-module **/
+import { Notification } from "@web/core/notifications/notification";
+import { patch } from "@web/core/utils/patch";
 
-
-patch(Notification.props, "webNotifyProps", {
+patch(Notification.props, {
     type: {
         type: String,
         optional: true,
--- mf_muslim_azkar/static/src/js/notification_services.esm.js
+++ mf_muslim_azkar/static/src/js/notification_services.esm.js
@@ -1,59 +1,65 @@
 /** @odoo-module **/
-import {browser} from "@web/core/browser/browser";
-console.log("browser", browser);
-import {registry} from "@web/core/registry";
+import { browser } from "@web/core/browser/browser";
+import { registry } from "@web/core/registry";
+import { EventBus } from "@odoo/owl";
 
 export const webNotificationService = {
     dependencies: ["action", "bus_service", "notification"],
 
-    start(env, {action, bus_service, notification}) {
+    start(env, { action, bus_service, notification }) {
+        const bus = new EventBus();
         let webNotifTimeouts = {};
-        const azkarSound = new Audio('mf_muslim_azkar/static/src/sound/door-knock.mp3'); // Add the path to your sound file
-        const salahSound = new Audio('mf_muslim_azkar/static/src/sound/salah.mp3'); // Add the path to your sound file
 
-        bus_service.addEventListener('notification', ({ detail: notifications }) => {
-            for (const {payload, type} of notifications) {
-                console.log("payload", payload);
-                console.log("type", type);
-                if (type === "muslim.azkar") {
-                    playNotificationSound(); // Play sound
-                    displaywebNotification(payload);
-                }
-                if (type === "muslim.azan") {
-                    playAzanSound(); // Play sound
-                    displaywebNotification(payload);
-                }
-            }
-        });
-        bus_service.start();
+        const azkarSound = new Audio('/mf_muslim_azkar/static/src/sound/door-knock.mp3');
+        const salahSound = new Audio('/mf_muslim_azkar/static/src/sound/salah.mp3');
 
         function playNotificationSound() {
             azkarSound.play().catch((error) => {
                 console.error("Notification sound failed to play:", error);
             });
         }
+
         function playAzanSound() {
             salahSound.play().catch((error) => {
-                console.error("Notification sound failed to play:", error);
+                console.error("Azan sound failed to play:", error);
             });
         }
 
-        function displaywebNotification(notifications) {
-            Object.values(webNotifTimeouts).forEach((notif) =>
-                browser.clearTimeout(notif)
-            );
+        function displayWebNotification(notifications) {
+            Object.values(webNotifTimeouts).forEach((notif) => browser.clearTimeout(notif));
             webNotifTimeouts = {};
-            notifications.forEach(function (notif) {
-                browser.setTimeout(function () {
+            notifications.forEach((notif, index) => {
+                webNotifTimeouts[index] = browser.setTimeout(() => {
                     notification.add(notif.message, {
                         title: notif.title,
                         type: notif.type,
                         sticky: notif.sticky,
-                        className: notif.className
+                        className: notif.className,
                     });
-                });
+                }, 0);
             });
         }
+
+        bus_service.addEventListener('notification', ({ detail: notifications }) => {
+            for (const { payload, type } of notifications) {
+                if (type === "muslim.azkar") {
+                    playNotificationSound();
+                    displayWebNotification(payload);
+                } else if (type === "muslim.azan") {
+                    playAzanSound();
+                    displayWebNotification(payload);
+                }
+            }
+        });
+
+        bus_service.start();
+
+        return {
+            addEventListener: bus.addEventListener.bind(bus),
+            removeEventListener: bus.removeEventListener.bind(bus),
+            trigger: bus.trigger.bind(bus),
+        };
     },
 };
+
 registry.category("services").add("webNotification", webNotificationService);
